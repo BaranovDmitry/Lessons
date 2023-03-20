5f168d73130458248cd0f1baabea43ce3ac758ea
BaranovDmitry <dmitrybaranov1992@gmail.com>
2023-03-20 23:46:02 +0200

    3rd homework. Умовні вирази/

---
+++ 3rd_homework.php
@@ -0,0 +1,21 @@
+<?php
+//First Task
+$value = 'en';
+if($value === 'en'){
+    echo "Baranov Dmytro";
+} else {
+    echo "Баранов Дмитро";
+}
+
+
+echo'<br>';
+echo'<br>';
+//Second Task
+$var = 5;
+
+if($var > 0 && $var < 6){
+    echo 'true';
+} else {
+    echo 'false';
+}
+//Якщо значення 5.0 - то "true", якщо значення -3.2 - то "false"
